5da1acf12cad98e23ee1b055d9f447c6ebad61f1
Craiii24 <kentreyes208@gmail.com>
2026-06-14 19:53:12 +0800

    Learning materials

---
+++ Loop/while1.py
@@ -0,0 +1,7 @@
+#WHILE LOOP PASSWORD
+
+user_password = ''
+while user_password != 'kentii':
+    print("Wrong password, try again.")
+    user_password = input("Enter the password: ")
+print("Welcome to the system!")
